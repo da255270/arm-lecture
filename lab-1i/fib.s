ce5ed4ad442e3ca633ac9d865ad88ec617d3ba31
da255270 <da255270@hotmail.com>
2015-10-05 21:49:22 +0800

    非遞迴費氏數列
    把註解都拿掉並且參照投影片p72的演算法寫非遞迴費氏數列

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -12,32 +12,23 @@ fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
 	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
+	push {r3, r4, r5, r6, lr}
+        mov r3,#0xFFFFFFFF
+        mov r4,#0
+        mov r5,#0
+        mov r6,#1
+loop:      
+        add r4,r6,r3	
+        mov r3,r6
+        mov r6,r4
+        add r5,r5,#1
+        cmp r5,r0
+        ble loop
+        mov r0,r6
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
 
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
